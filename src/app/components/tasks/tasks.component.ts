b59d07d1ecc2a67a18ef3456184bfe6d28407e92
Gerson Umanzor Alberto <gbumanzor@outlook.com>
2019-11-09 22:09:29 -0600

    Added remove task functionality and also the creation date

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -9,31 +9,36 @@ styleUrls: ['./tasks.component.css']
 
 export class TasksComponent implements OnInit {
 
-    tasks:Task;
+    tasks:Task[];
 
     constructor() {
-        /*
+
+        let date = new Date();
+        
         let temptasks = [
             {
                 id: 1,
                 task: "Take out the trash",
                 assignee: "John",
+                creation_date: date.toLocaleDateString() + " " + date.toLocaleTimeString(),
                 status: false
             },
             {
                 id: 2,
                 task: "Wash the bathroom",
                 assignee: "Laura",
+                creation_date: date.toLocaleDateString() + " " + date.toLocaleTimeString(),
                 status: false
             },
             {
                 id: 3,
                 task: "Make the bed",
                 assignee: "",
+                creation_date: date.toLocaleDateString() + " " + date.toLocaleTimeString(),
                 status: false
             },
         ];
-        localStorage.setItem("tasks", JSON.stringify(temptasks)); */
+        localStorage.setItem("tasks", JSON.stringify(temptasks));
 
         this.tasks = this.showTasks()
      }
@@ -44,6 +49,21 @@ export class TasksComponent implements OnInit {
 
     showTasks() {
         return JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    removeTask(task:Task) {
+        if(confirm("Are you sure you want to remove this task?")) {
+
+            //Delete from LocalStorage
+            let mytasks = JSON.parse(localStorage.getItem("tasks")).filter((t) => {
+                return t.id != task.id;
+            });
+
+            //Show the updated list.
+            localStorage.setItem("tasks", JSON.stringify(mytasks));
+            this.tasks = this.showTasks();
+
+        }
     }
 
 }
--- src/app/models/Task.ts
+++ src/app/models/Task.ts
@@ -2,5 +2,6 @@ export class Task {
     id?:number;
     task:string;
     assignee:string;
+    creation_date:string;
     status:boolean;
 }
