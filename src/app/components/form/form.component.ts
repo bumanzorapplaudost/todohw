de30336c42524f9903377d00cd362b530cbc9df2
Gerson Umanzor Alberto <gbumanzor@outlook.com>
2019-11-10 22:13:53 -0600

    Adding form functionality

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+    description:string;
+    assignee:string;
+    status:boolean;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addTodo() {
+      let date = new Date();
+      let task = {
+          id: Math.random() * 500,
+          task: this.description,
+          assignee: this.assignee,
+          creation_date: date.toLocaleDateString() + " " + date.toLocaleTimeString(),
+          status: this.status,
+      }
   }
 
 }
