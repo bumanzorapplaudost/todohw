f45550ab88b2fb44aebc3970062a8712d44873af
Gerson Umanzor Alberto <gbumanzor@outlook.com>
2019-11-10 22:44:21 -0600

    The form already saves, pending on show the tasks list

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,30 +1,49 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+    selector: 'app-form',
+    templateUrl: './form.component.html',
+    styleUrls: ['./form.component.css']
 })
+
 export class FormComponent implements OnInit {
 
-    description:string;
+    description:string = "";
     assignee:string;
     status:boolean;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  addTodo() {
-      let date = new Date();
-      let task = {
-          id: Math.random() * 500,
-          task: this.description,
-          assignee: this.assignee,
-          creation_date: date.toLocaleDateString() + " " + date.toLocaleTimeString(),
-          status: this.status,
-      }
-  }
+    addTodo() {
+        let date = new Date();
 
+        if(this.description.length <= 100 && this.description != "") {
+
+            document.querySelector("#description").classList.remove("is-invalid");
+            let task = {
+                id: Math.round(Math.random() * 500),
+                task: this.description,
+                assignee: this.assignee,
+                creation_date: date.toLocaleDateString() + " " + date.toLocaleTimeString(),
+                status: this.status,
+            }
+
+            let currentTasks = JSON.parse(localStorage.getItem("tasks"));
+            currentTasks.push(task);
+            localStorage.setItem("tasks", JSON.stringify(currentTasks));
+
+        }else {
+            document.querySelector("#description").classList.add("is-invalid");
+
+            if(this.description.trim() == "") {
+                document.querySelector("#itd").innerHTML = "The task description field is required."; 
+            }
+            if(this.description.length > 100) {
+                document.querySelector("#itd").innerHTML = "The field <b>must</b> be 100 characters or less.";
+            }
+        }
+    }
 }
